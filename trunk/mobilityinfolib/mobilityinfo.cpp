7dd0809b760e1663990ae31fc317bf8dd4214e79
Attila Csipa <git@csipa.in.rs>
2011-04-04 20:44:55 +0300

    network interfaces HW features

--- trunk/mobilityinfolib/mobilityinfo.cpp
+++ trunk/mobilityinfolib/mobilityinfo.cpp
@@ -23,5 +23,25 @@ QList<QPair<QString, QString> > mobilityInfo()
         value = si.version((QSystemInfo::Version)4);
     info.append(QPair<QString,QString>("Version", value));
 
+    QStringList valuelist;
+    QSystemInfo qsi;
+    if (qsi.hasFeatureSupported(QSystemInfo::BluetoothFeature)) valuelist << "Bluetooth";
+    if (qsi.hasFeatureSupported(QSystemInfo::CameraFeature)) valuelist << "Camera";
+    if (qsi.hasFeatureSupported(QSystemInfo::FmradioFeature)) valuelist << "FM Radio";
+    if (qsi.hasFeatureSupported(QSystemInfo::IrFeature)) valuelist << "Infrared";
+    if (qsi.hasFeatureSupported(QSystemInfo::LedFeature)) valuelist << "LED";
+    if (qsi.hasFeatureSupported(QSystemInfo::MemcardFeature)) valuelist << "Memory card";
+    if (qsi.hasFeatureSupported(QSystemInfo::UsbFeature)) valuelist << "USB";
+    if (qsi.hasFeatureSupported(QSystemInfo::VibFeature)) valuelist << "Vibra";
+    if (qsi.hasFeatureSupported(QSystemInfo::WlanFeature)) valuelist << "WiFi";
+    if (qsi.hasFeatureSupported(QSystemInfo::SimFeature)) valuelist << "SIM";
+    if (qsi.hasFeatureSupported(QSystemInfo::LocationFeature)) valuelist << "GPS";
+    if (qsi.hasFeatureSupported(QSystemInfo::VideoOutFeature)) valuelist << "Video out";
+    if (qsi.hasFeatureSupported(QSystemInfo::HapticsFeature)) valuelist << "Haptics";
+    if (qsi.hasFeatureSupported((QSystemInfo::Feature)13)) valuelist << "FM Transmitter"; // added in 1.2 so no enum
+
+    info.append(QPair<QString,QString>("Hardware features", valuelist.join(", ")));
+    info.append(QPair<QString,QString>("Available languages", qsi.availableLanguages().join(", ")));
+
     return info;
 }
--- trunk/webkitinfolib/webkitinfo.cpp
+++ trunk/webkitinfolib/webkitinfo.cpp
@@ -17,6 +17,17 @@ QList<QPair<QString, QString> > webkitInfo()
     //  QtWebKit tries to load platformplugin.dll on startup, and if that is
     //  found, it will be used for certain platform-specific operations.
 
+    QStringList valuelist;
+    info.append(QPair<QString,QString>("section", "Networking"));
+    foreach (QNetworkInterface qni, QNetworkInterface::allInterfaces () ) {
+        if (!qni.addressEntries().isEmpty())
+            info.append(QPair<QString,QString>(qni.name(), QString("%0 - %1").arg(qni.hardwareAddress()).arg(qni.addressEntries().at(0).ip().toString())));
+        else
+            info.append(QPair<QString,QString>(qni.name(), qni.hardwareAddress()));
+    }
+
+    // 4.7 functionality, for an eventual networkinfo plugin
+    // QList<QNetworkConfiguration> QNetworkConfigurationManager::allConfigurations
 
     return info;
 }
